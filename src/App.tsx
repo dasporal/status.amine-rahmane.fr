8584f917f9da7c00616898657926a8e6830e2aed
dasporal <dasporal@gmail.com>
2024-01-07 16:34:39 +0100

    chore: configured prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
 function App() {
   return (
     <main className="min-h-screen bg-background">
-      <h1 className="text-text text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl  font-bold text-text underline">Hello world!</h1>
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
