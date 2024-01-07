5230697f6e0b9a4b78b007c0a15a44712e20f8ab
dasporal <dasporal@gmail.com>
2024-01-07 22:19:35 +0100

    feat(frontend): pull data from api and display it

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import { SiteCard } from './components/SiteCard'
 
 function App() {
+  // Write an api call
+  const [data, setData] = useState([])
+  useEffect(() => {
+    fetch('/api/v1/status')
+      .then((response) => response.json())
+      .then((data) => setData(data))
+  }, [])
+
   return (
     <main className="bg-background-950 min-h-screen">
       <div className="container py-4">
         <h1 className="text-3xl font-bold text-white">Status Page</h1>
         <div className="grid auto-rows-fr grid-cols-1 md:grid-cols-3  gap-4 p-4">
-          <SiteCard />
+          {
+            data.map((site: any) => (
+              <SiteCard key={site.Website.websiteID} data={site} />
+            ))
+          }
         </div>
       </div>
     </main>
--- src/components/SiteCard.tsx
+++ src/components/SiteCard.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import {
   Card,
   CardHeader,
@@ -6,19 +7,47 @@ import {
   CardContent,
 } from './ui/card'
 
-export function SiteCard() {
+export function SiteCard({ data }) {
+  const [status, setStatus] = useState({
+    text: 'Offline',
+    color: 'text-red-500',
+  })
+  const [averageResponseTime, setAverageResponseTime] = useState(0)
+
+  useEffect(() => {
+    const totalResponseTime = data.StatusChecks.reduce((acc, check) => acc + check.ResponseTime, 0);
+    const avgResponseTime = totalResponseTime / data.StatusChecks.length;
+    setAverageResponseTime(Math.trunc(avgResponseTime));
+
+
+    if (
+      data.StatusChecks[0].StatusCode >= 200 &&
+      data.StatusChecks[0].StatusCode <= 299
+    ) {
+      setStatus({ text: 'Online', color: 'text-green-500' })
+    } else if (
+      data.StatusChecks[0].StatusCode >= 400 &&
+      data.StatusChecks[0].StatusCode <= 499
+    ) {
+      setStatus({ text: 'Website not foud', color: 'text-red-500' })
+    } else {
+      setStatus({ text: 'Offline', color: 'text-red-500' })
+    }
+  }, [data])
+
   return (
     <Card className="hover:bg-background-900">
-      <a href="https://amine-rahmane.fr">
+      <a href={data.Website.Url}>
         <CardHeader>
-          <CardTitle>Amine Rahmane</CardTitle>
-          <CardDescription className="font-semibold text-green-500">
-            Online
+          <CardTitle>{data.Website.Name}</CardTitle>
+          <CardDescription className={`flex gap-4 font-semibold ${status.color}`}>
+            {status.text}
+            {averageResponseTime > 1000 && (<span className='text-orange-500'>Performance degraded</span>)}
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <p>Average response time today: 340ms</p>
-          <p>Last Reponse: 200 OK</p>
+          <p>Average response time today: {averageResponseTime}ms</p>
+          <p>Last Reponse: {data.StatusChecks[0].Status}</p>
         </CardContent>
       </a>
     </Card>
