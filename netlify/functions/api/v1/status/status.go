e6080e3fdf6b19737e92bd7a7c385b9be87c31ac
dasporal <dasporal@gmail.com>
2024-01-07 20:10:02 +0100

    feat(function): readded monitoring to main function + working on api

--- netlify/functions/api/v1/status/status.go
+++ netlify/functions/api/v1/status/status.go
@@ -1,13 +1,13 @@
+// This function is called to retrieve the logs of the day?
+
 package main
 
 import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/dasporal/status.amine-rahmane.fr/pkg/monitoring"
 )
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-  monitoring.PingWebsites()
 	return &events.APIGatewayProxyResponse{
     StatusCode:        200,
     Body:              "Function called and websites pinged",
